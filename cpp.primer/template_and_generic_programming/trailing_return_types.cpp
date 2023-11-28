a26cbf156c235e7c470558a37ea764c1f1dbae4c
fansuregrin <pwz113436@gmail.com>
2023-11-28 21:24:31 +0800

    trailing return types

---
+++ cpp.primer/template_and_generic_programming/trailing_return_types.cpp
@@ -0,0 +1,20 @@
+// topic: trailing return types (尾置返回类型)
+
+#include <vector>
+
+using namespace std;
+
+// a trailing return lets us declare the return type after the parameter list is seen
+
+template<typename It>
+auto fcn(It beg, It end) -> decltype(*beg) {
+    for (It i=beg; i!=end; ++i) {
+        ++*i;
+    }
+    return *beg;  // return a reference to an element from the range
+}
+
+int main() {
+    vector<int> vec{1, 2, 3};
+    fcn(vec.begin(), vec.end());
+}
